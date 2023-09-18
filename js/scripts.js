430920d77394bbbad0dc90d0261a3395fe5e5812
trilby4 <143022808+trilby4@users.noreply.github.com>
2023-09-17 23:29:15 -0700

    updated Styling to remove unnecessary margins, and code, updated html col to be correct for design, updated js to remove pop-up

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,1 @@
-alert("Welcome to my coding protfolio, I hope you enjoy! Visit Trilby4.com for my design portfolio and see the differences.");
+console.log("Welcome to my coding protfolio, I hope you enjoy! Visit Trilby4.com for my design portfolio and see the differences.");
