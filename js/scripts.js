a7d265847ca39bebb919422f0aa5f6724140ed5f
trilby4 <143022808+trilby4@users.noreply.github.com>
2023-09-26 09:34:32 -0700

    Added JS per 2.3, fixed div placement for container class on home page, added p tag for contact button on about me page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Welcome to my coding protfolio, I hope you enjoy! Visit Trilby4.com for my design portfolio and see the differences.");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
