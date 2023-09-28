e8a54d8e56cf9d9f6369d6727c6132a0104a5b00
trilby4 <143022808+trilby4@users.noreply.github.com>
2023-09-28 10:46:32 -0700

    updated header, link colors, button link, hamburger menu changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-console.log("Welcome to my coding protfolio, I hope you enjoy! Visit Trilby4.com for my design portfolio and see the differences.");
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
